2bae41b685d613da05dc4d37ad2b95784381e3bd
Mohammed <mohamed734525@gmail.com>
2025-04-04 02:23:17 +0300

    adding the admin psnnel of colleges model

--- universityApps/colleges/admin.py
+++ universityApps/colleges/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import College
 
-# Register your models here.
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+     list_display = ('college_no','code', 'name', 'description')
+     search_fields = ('college_no','code', 'name', 'description')
+     ordering = ['code','college_no']
+     list_per_page = 10
+     list_max_show_all = 100
+     
