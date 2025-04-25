862c20443899ad408811f570954ccf0d2809188b
Mohammed <mohamed734525@gmail.com>
2025-04-25 05:37:03 +0300

    Admission admin

--- universityApps/admissions/admin.py
+++ universityApps/admissions/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import AdmissionApplication, ApplicationDocument
 
-# Register your models here.
+
+class ApplicationDocumentInline(admin.TabularInline):
+    model = ApplicationDocument
+    extra = 0
+    readonly_fields = ('document_type', 'title', 'file', 'uploaded_at')
+    can_delete = False
+
+
+@admin.register(AdmissionApplication)
+class AdmissionApplicationAdmin(admin.ModelAdmin):
+    list_display = (
+        'full_name',
+        'email',
+        'phone_number',
+        'program',
+        'status',
+        'submission_date',
+        'reviewed_by',
+        'reviewed_date'
+    )
+    list_filter = ('status', 'program', 'archived')
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number', 'national_id')
+    readonly_fields = ('submission_date', 'reviewed_date', 'reviewed_by')
+    inlines = [ApplicationDocumentInline]
+    ordering = ('-submission_date',)
+
+    def full_name(self, obj):
+        return obj.full_name()
+    full_name.short_description = "Full Name"
+
+
+@admin.register(ApplicationDocument)
+class ApplicationDocumentAdmin(admin.ModelAdmin):
+    list_display = (
+        'get_applicant_name',
+        'document_type',
+        'title',
+        'uploaded_at',
+    )
+    list_filter = ('document_type',)
+    search_fields = ('application__first_name', 'application__last_name', 'title')
+
+    def get_applicant_name(self, obj):
+        return f"{obj.application.first_name} {obj.application.last_name}"
+    get_applicant_name.short_description = "Applicant Name"
